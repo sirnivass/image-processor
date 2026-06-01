6dbf8e885f529d2aad9974122668ab1d64e107a7
Rodolffo Nivass <79434109+sirnivass@users.noreply.github.com>
2026-06-01 14:07:16 -0300

    Cria o setup inicial do Spring Boot
    Cria o setup inicial do Spring Boot e adiciona uma imagem de proposta de Git Flow

---
+++ architects/src/test/java/com/cata/architects/ArchitectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cata.architects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
