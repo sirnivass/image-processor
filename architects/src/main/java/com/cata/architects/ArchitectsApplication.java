6ba8f6e2f9365b91ecfb77abd39a920494a906a2
RodolffoNMS <sirnivass@gmail.com>
2026-06-01 12:06:24 -0300

    Cria o setup inicial do Spring Boot

---
+++ architects/src/main/java/com/cata/architects/ArchitectsApplication.java
@@ -0,0 +1,13 @@
+package com.cata.architects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectsApplication.class, args);
+	}
+
+}
